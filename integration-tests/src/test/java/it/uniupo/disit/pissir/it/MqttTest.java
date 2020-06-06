966533a23f7c4678901d0af68b11d35862b7cdab
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2020-06-06 20:15:30 +0200

    First test using docker-compose

--- integration-tests/src/test/java/it/uniupo/disit/pissir/it/MqttTest.java
+++ integration-tests/src/test/java/it/uniupo/disit/pissir/it/MqttTest.java
@@ -1,5 +1,7 @@
 package it.uniupo.disit.pissir.it;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.paho.client.mqttv3.*;
 import org.junit.Test;
 
 import java.io.File;
@@ -7,16 +9,20 @@ import java.net.URL;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.junit.Assert.*;
 
 public class MqttTest {
-
     CsvParser parser = new CsvParser();
     ClassLoader classLoader = getClass().getClassLoader();
+    ObjectMapper objectMapper = new ObjectMapper();
+    int messageCounter = 0;
 
     @Test
     public void parseCsv() throws Exception {
         URL resource = classLoader.getResource("small.csv");
+        assertNotNull(resource);
         File file = new File(resource.getFile());
 
         Stream<Csv> csvStream = parser.csvLines(file)
@@ -26,4 +32,55 @@ public class MqttTest {
         assertEquals(71, csvStream.count());
     }
 
+    @Test
+    public void publish() throws Exception {
+        String brokerURL = "tcp://localhost:1883";
+
+        MqttClient clientPublisher = new MqttClient(brokerURL, MqttClient.generateClientId());
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setCleanSession(false);
+        clientPublisher.connect(options);
+        MqttTopic testTopic = clientPublisher.getTopic("test");
+
+        MqttClient clientSubscriber = new MqttClient(brokerURL, MqttClient.generateClientId());
+        clientSubscriber.setCallback(new SubscribeCallback());
+        clientSubscriber.connect();
+        clientSubscriber.subscribe("test");
+
+        URL resource = classLoader.getResource("small.csv");
+        assertNotNull(resource);
+        File file = new File(resource.getFile());
+
+        Stream<Csv> csvStream = parser.csvLines(file)
+                .map(line -> parser.parse(line))
+                .flatMap(Optional::stream);
+
+        csvStream.forEach(csv -> {
+            try {
+                byte[] payload = objectMapper.writeValueAsBytes(csv);
+                testTopic.publish(new MqttMessage(payload));
+            } catch (Exception e) {
+                fail();
+            }
+        });
+
+        await().atMost(5, SECONDS).until(() -> messageCounter == 71);
+        assertEquals(71, messageCounter);
+    }
+
+    class SubscribeCallback implements MqttCallback {
+
+        @Override
+        public void connectionLost(Throwable cause) {
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) {
+            messageCounter++;
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+        }
+    }
 }
