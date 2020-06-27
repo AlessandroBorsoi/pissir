87d5245894ae0ee457642557821d92c8a0d5e8b8
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2020-06-27 19:17:32 +0200

    Working mqtt to kafka to mongodb

--- start.sh
+++ start.sh
@@ -3,4 +3,13 @@
 printf "Running docker compose...\n"
 docker-compose -f ./docker/service-architecture/docker-compose.yml up -d
 
+echo "Waiting for connectors up"
+until $(curl --output /dev/null --silent --head --fail localhost:8083/connectors); do
+    printf '.'
+    sleep 5
+done
+printf "\nUp\n"
 
+printf "Configuring connectors...\n"
+printf "MongoDB sink connector...\n"
+curl -d @./docker/service-architecture/connect-mongodb-sink.json -H "Content-Type: application/json" -X POST http://localhost:8083/connectors
