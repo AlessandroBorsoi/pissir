4da9ff189efce51b6b8148b0d5408a48a085fa17
AlessandroBorsoi <alessandro.borsoi1984@gmail.com>
2020-07-01 10:12:09 +0200

    Feature/docker services (#4)
    Dockerizing services

--- start-service-arch.sh
+++ start-service-arch.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+printf "Building service...\n"
+gradle clean :mqtt-ingestion-service:build :mqtt-ingestion-service:docker
+
 printf "Running docker compose...\n"
 docker-compose -f ./docker/service-architecture/docker-compose.yml up -d
 
@@ -13,9 +16,3 @@ printf "\nUp\n"
 printf "Configuring connectors...\n"
 printf "MongoDB sink connector...\n"
 curl -d @./docker/connect-mongodb-sink.json -H "Content-Type: application/json" -X POST http://localhost:8083/connectors
-
-printf "Building service...\n"
-gradle clean :mqtt-ingestion-service:build
-
-printf "starting service...\n"
-java -jar service/build/libs/service-0.1-all.jar
